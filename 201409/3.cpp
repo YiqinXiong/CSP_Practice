bdeaf4e359ea9748ce0238f47de591269a6ef11a
YiqinXiong <u201714501@hust.edu.cn>
2020-09-03 20:13:25 +0800

    pass 201409-3

---
+++ 201409/3.cpp
@@ -0,0 +1,53 @@
+/*
+*   用时 71分
+*   得分 90分
+*/
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string strUpper(string str)
+{
+    string upperStr = "";
+    for(int i=0; i<str.length(); i++)
+    {
+        upperStr.append(1,toupper(str[i]));
+    }
+    return upperStr;
+}
+
+int main()
+{
+    string sample;
+    int n;
+    int sensitive;
+    vector<string> lines;
+    cin >> sample;
+    cin >> sensitive;
+    if(!sensitive) sample=strUpper(sample);
+    //cout <<"sample: "<<sample<<endl;
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        string temp, upperTemp;
+        cin >> temp;
+        upperTemp = strUpper(temp);
+        //cout <<"upperTemp: "<<upperTemp<<endl;
+        if(!sensitive)
+        {
+            if(upperTemp.find(sample)!=string::npos)
+                cout << temp << endl;
+        }
+        else
+        {
+            if(temp.find(sample)!=string::npos)
+                cout << temp << endl;
+        }
+    }
+    return 0;
+}
